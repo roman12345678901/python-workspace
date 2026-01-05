964bdfddf4d312f030bb49582f3c7aedaddba4ba
Roman Yatsykovich <r.yatsykovich@gmail.com>
2026-01-05 16:59:38 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+debug_mode = os.getenv("DEBUG")
+name = os.getenv("MY_NAME")
+
+print (f"[ENV TEST] Hello, {name}")
+print (f"[ENV TEST] API key: {api_key}")
+print (f"[ENV TEST] Debug_mode: {debug_mode}")
+
+#FINISH
